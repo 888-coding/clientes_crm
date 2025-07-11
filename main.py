f5036c35471df40cde5e8a817339020116a092ab
888-coding <pachelee@gmail.com>
2025-07-11 00:11:27 -0300

    在main裡建立了目錄

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py 
+def main():
+    print("Clients Relationship management")
+    print("===============================")
+    print("1. Insert client")
+    print("2. List all clients")
+    print("3. Search client by name")
+    print("4. Search client by surname")
+    print("5. Delete client")
+    option_chosen = input("Choose option: ")    
+main()
